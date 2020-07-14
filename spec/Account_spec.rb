b71a58300068dc3bd818159171d14ebf31e3d960
J Perrin <perrinjack96@gmail.com>
2020-07-14 10:13:01 +0100

    RED new account starts with empty list of trasnsactions

--- spec/Account_spec.rb
+++ spec/Account_spec.rb
@@ -3,8 +3,12 @@
 require_relative '../lib/Account.rb'
 
 describe Account do
-let(:account) { Account.new }
+  let(:account) { Account.new }
   it 'Account.new creates instance of an account' do
     expect(account).to be_instance_of Account
+  end
+
+  it 'starts with an empty list of transactions' do
+    expect(account.transactions).to eq []
   end
 end
