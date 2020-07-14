16acfe6afbe9a0b19e0196f3bfd51a26e8f307a4
J Perrin <perrinjack96@gmail.com>
2020-07-14 10:41:44 +0100

    transaction is an instance of transaction class

--- spec/transaction_Spec.rb
+++ spec/transaction_Spec.rb
@@ -4,7 +4,7 @@ require_relative '../lib/transaction.rb'
 
 describe Account do
   let(:account) { double(:account) }
-
+  let(:transaction) { Transaction.new }
   it 'transaction is an instance of the Transaction class' do
     expect(transaction).to be_instance_of Transaction
   end
