37c63cbaf5ea4d9efb6465db06bb5b54a711cd55
J Perrin <perrinjack96@gmail.com>
2020-07-14 10:19:00 +0100

    GREEN account starts with balance of 0

--- lib/Account.rb
+++ lib/Account.rb
@@ -1,8 +1,9 @@
 # frozen_string_literal: true
 
 class Account
-  attr_reader :transactions
+  attr_reader :transactions, :balance
   def initialize
     @transactions = []
+    @balance = 0
 end
 end
