1ebc0c2054c4ee7e3025a828f5916767904d6a94
J Perrin <perrinjack96@gmail.com>
2020-07-14 11:11:45 +0100

    refactored typo of placeing withdrawals as credit transactions

--- lib/Account.rb
+++ lib/Account.rb
@@ -15,6 +15,6 @@ end
 
   def withdraw(amount)
     @balance -= amount
-    @transactions.push(Transaction.new(amount, 'credit'))
+    @transactions.push(Transaction.new(amount, 'debit'))
   end
 end
